5cebc573b9d25004309d3ca47d8aa614167fb701
devland <adevland@gmail.com>
2023-07-16 20:09:39 +0300

    first commit :)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# staticFileServer
+A simple, no dependency static file server that runs on NodeJS.  
+Start via `node staticFileServer.js` and point your browser to http://localhost:8080/path/to/yourFile.ext
---
+++ staticFileServer.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const port = process.argv[2] || 8080;
+const log = (item) => {
+  const now = new Date();
+  process.stdout.write(`[${now.getTime()}.${now.getMilliseconds()}]: `);
+  console.log(item);
+}
+http.createServer(async (request, response) => {
+  try {
+    const benchmarkStart = new Date();
+    const parts = new URL(`http://localhost:${port}${request.url}`).pathname.split('/');
+    const pieces = [];
+    for (let item of parts)
+      if (item) pieces.push(item);
+    const path = pieces.join('/');
+    const result = fs.readFileSync(path, 'binary');
+    response.writeHead(200);
+    response.write(result, 'binary');
+    response.end();
+    const benchmarkEnd = new Date();
+    log(`${path} (${(benchmarkEnd - benchmarkStart)} ms)`);
+  }
+  catch (error) {
+    log(error);
+    response.writeHead(500);
+    response.write(error.toString(), 'binary');
+    response.end();
+  }
+}).listen(parseInt(port, 10));
+log(`static file server running at http://localhost: ${port}`);
