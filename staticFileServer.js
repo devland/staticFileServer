59cc9822682809afbb24281b1576d16580c92279
devland <adevland@gmail.com>
2025-09-20 13:19:50 +0300

    add mime types

--- staticFileServer.js
+++ staticFileServer.js
@@ -1,21 +1,37 @@
 const http = require('http');
 const fs = require('fs');
+const mimes = require('./mimes.js');
 const port = process.argv[2] || 8080;
 const log = (item) => {
   const now = new Date();
   process.stdout.write(`[${now.getTime()}.${now.getMilliseconds()}]: `);
   console.log(item);
 }
+const getMimeType = (extension) => {
+  for (let item of mimes) {
+    if (item.extensions.includes(extension)) {
+      return item.type;
+    }
+  }
+  return null;
+}
 http.createServer((request, response) => {
   try {
     const benchmarkStart = new Date();
     const parts = new URL(`http://localhost:${port}${request.url}`).pathname.split('/');
     const pieces = [];
-    for (let item of parts)
+    for (let item of parts) {
       if (item) pieces.push(item);
+    }
     const path = pieces.join('/');
+    const extension = '.' + pieces.pop().split('.').pop();
     const result = fs.readFileSync(path, 'binary');
-    response.writeHead(200);
+    const headers = {};
+    const mimeType = getMimeType(extension);
+    if (mimeType) {
+      headers['Content-Type'] = mimeType;
+    }
+    response.writeHead(200, headers);
     response.write(result, 'binary');
     response.end();
     const benchmarkEnd = new Date();
