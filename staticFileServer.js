d396753e42b0a18f986fd6a09fb9e290242d8adc
devland <adevland@gmail.com>
2026-05-03 13:01:48 +0300

    refactor mime types

--- staticFileServer.js
+++ staticFileServer.js
@@ -1,19 +1,14 @@
 const http = require('http');
 const fs = require('fs');
-const mimes = require('./mimes.js');
+const { execSync } = require("child_process");
 const config = require(process.argv[2] || './config.sample.js');
 const log = (item) => {
   const now = new Date();
   process.stdout.write(`[${now.toISOString()}]: `);
   console.log(item);
 }
-const getMimeType = (extension) => {
-  for (let item of mimes) {
-    if (item.extensions.includes(extension)) {
-      return item.type;
-    }
-  }
-  return null;
+const getMimeType = (path) => {
+  return execSync(`mimetype -b ${path}`).toString().replace('\n', '');
 }
 http.createServer((request, response) => {
   try {
@@ -25,22 +20,26 @@ http.createServer((request, response) => {
         pieces.push(item);
       }
     }
-    let path = pieces.join('/');
-    let extension = '.' + pieces.pop().split('.').pop();
+    let path = config.base + pieces.join('/');
     let result;
     const headers = {};
     let httpCode = 200;
-    if (fs.existsSync(config.base + path)) {
-      result = fs.readFileSync(config.base + path, 'binary');
+    if (fs.existsSync(path)) {
+      result = fs.readFileSync(path, 'binary');
     }
     else {
-      path = config['404'];
-      result = fs.readFileSync(config.base + path, 'binary');
-      extension = '.' + config['404'].split('/').pop().split('.').pop();
-      headers['Location'] = '/404.html';
-      httpCode = 301;
+      log(`${path} not found`);
+      path = config.base + config['404'];
+      if (fs.existsSync(path)) {
+        result = fs.readFileSync(path, 'binary');
+        headers['Location'] = '/404.html';
+        httpCode = 301;
+      }
+      else {
+        throw '404_file_not_found';
+      }
     }
-    const mimeType = getMimeType(extension);
+    const mimeType = getMimeType(path);
     if (mimeType) {
       headers['Content-Type'] = mimeType;
     }
